a1bd55f0dfa1dfe9594272d270f9538b6b9a7c06
hmc-mouse-s18 <scarlson@cs.hmc.edu>
2018-09-27 14:18:08 +0200

    Created Game + added basic movement

---
+++ GhostGame/Assets/Scripts/GhostController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostController : MonoBehaviour {
+
+    private Rigidbody2D rb;
+    private SpriteRenderer mySpriteRenderer;
+
+    public Sprite rightSprite;
+    public Sprite leftSprite;
+    public Sprite stillSprite;
+
+    // Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+	}
+	
+
+    //Fixed Update is called before any physics operations
+    private void FixedUpdate()
+    {
+        // Make the ghost move with the arrow keys
+        float moveH = Input.GetAxis("Horizontal");
+        float moveV = Input.GetAxis("Vertical");
+
+        Vector2 vel = rb.velocity;
+        vel.x = moveH;
+        vel.y = moveV;
+        rb.velocity = vel;
+
+        //Make the ghost look the correct direction when moving
+        if (vel.x > 0) {
+            mySpriteRenderer.sprite = rightSprite;
+        }
+        else if (vel.x < 0) {
+            mySpriteRenderer.sprite = leftSprite;
+        }
+        else {
+            mySpriteRenderer.sprite = stillSprite;
+        }
+
+    }
+}
