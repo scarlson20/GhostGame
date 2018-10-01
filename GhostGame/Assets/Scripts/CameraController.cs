a2dc5957c3a8f153bcbf3915aa4a52fef272fa3d
hmc-mouse-s18 <scarlson@cs.hmc.edu>
2018-10-01 11:09:19 +0200

    Camera Movement Implemented

---
+++ GhostGame/Assets/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Transform ghost;
+
+    public float offsetX;
+    public float offsetY;
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// This was taken from FlappyChicken and modified
+    /// </summary>
+    void Update()
+    {
+        if ((transform.position.x - ghost.position.x != offsetX) || (transform.position.y - ghost.position.y != offsetY))
+        {
+            Vector3 pos = transform.position;
+            pos.x = ghost.position.x + offsetX;
+            pos.y = ghost.position.y + offsetY;
+            transform.position = pos;
+        }
+    }
+
+
+}
